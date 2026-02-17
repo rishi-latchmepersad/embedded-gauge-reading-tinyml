b397ae4ddb2de4e3e3815740eafc58c37fbc6431
Rishi Latchmepersad <latchmepersad@gmail.com>
2026-02-17 14:16:20 -0400

    more integrations

--- firmware/stm32/n657/Appli/Inc/app_azure_rtos_config.h
+++ firmware/stm32/n657/Appli/Inc/app_azure_rtos_config.h
@@ -43,9 +43,9 @@ extern "C" {
 
 #define USE_STATIC_ALLOCATION                    1
 
-#define TX_APP_MEM_POOL_SIZE                     1024
+#define TX_APP_MEM_POOL_SIZE                     32764
 
-#define FX_APP_MEM_POOL_SIZE                     1024
+#define FX_APP_MEM_POOL_SIZE                     32000
 
 /* USER CODE BEGIN EC */
 
